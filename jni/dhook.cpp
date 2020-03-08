d28134688c233488fb1c78f80b2de6ad43e9d415
Matias Pavan <matiaspavan@gmail.com>
2020-03-08 16:50:00 -0300

    More refactoring

--- jni/dhook.cpp
+++ jni/dhook.cpp
@@ -12,55 +12,51 @@
 
 */
 byte ldr_pc_pc_4[4] = { 0x04, 0xF0, 0x1F, 0xE5 }; 
-
-
-
-void copyJumpToTargetBytes(byte * bytes, p_void target) {
+void copyBranchBytes(byte * bytes, p_void target) {
     for(int i = 0; i < 4; i++) {
         bytes[i] = ldr_pc_pc_4[i]; 
         bytes[i + 4] = ((uint32_t)target >> i * 8) & 0xFF; 
     }
 }
 
+// Set @protection flags on @target starting page, @return mprotect response parsed as MEMPROTECT_STATUS
 MEMPROTECT_STATUS setPageProtection(void * target, int protection) {
     p_void page_start = (p_void)((uint32_t)target - (uint32_t)target % PAGE_SIZE);
     return (MEMPROTECT_STATUS) mprotect((p_void)page_start, PAGE_SIZE, protection);
 }
 
-
+// Set @len bytes on @dest from @source
 void placeBytes(void * source, void *  dest, int len) {
     for(int i = 0; i < len; i++) *(byte *)((uint32_t)dest + i) = *(byte *)((uint32_t)source + i);
 }
 
-p_void DHookTrampoline(p_void origin, p_void detour) {
+p_void DHookTrampoline(p_void origin, p_void target) {
     DContainer * dhc = (DContainer *)malloc(sizeof(DContainer)); 
-    LOGD("DDBG", "::Ready to Hook: %p, Detour currently at: %p", origin, detour);
-    copyJumpToTargetBytes((byte*) dhc->branchDetourBytes, detour);
+    LOGD("DDBG", "::Ready to Hook: %p, Detour currently at: %p", origin, target);
+
+    copyBranchBytes(dhc->branchDetourBytes, target);
     placeBytes(origin, dhc->restorationBytes, 8); // Save first 8 bytes (2 instructions) from the original function to restore later in the trampoline
     LOGD("DDBG", "::Saved Original Bytes");
-    
+
     MEMPROTECT_STATUS status = setPageProtection(origin, PROT_READ | PROT_WRITE | PROT_EXEC);
     if(status == MEMPROTECT_FAILED) {
-        LOGE("DERR", "::mprotect failed step 1");
+        LOGE("DERR", "::mprotect failed on step 1");
         return NULL;
     }
-    
+
     placeBytes(dhc->branchDetourBytes, origin, 8); // Place the jump to our detour function on origin
     LOGD("DDBG", "::Placed Detour");
 
-    uint32_t returnAddress = ((uint32_t)origin + 0x8); // This is were we want to return after we finish with our detour code 
-
-    // dhc->restorationBytes contain the original function bytes in the first 8 bytes, the remaining 8 bytes are used to jump back to origin+0x8
-    copyJumpToTargetBytes((byte*)((uint32_t)dhc->restorationBytes + 8), (p_void)returnAddress); 
+    p_void returnAddress = (p_void)((uint32_t)origin + 0x8); // This is were we want to return after we finish with our detour code 
+    copyBranchBytes(dhc->restorationBytes + 8, returnAddress); // dhc->restorationBytes contain the original function bytes in the first 8 bytes, the remaining 8 bytes are used to jump back to origin+0x8
     status = setPageProtection((void *)dhc->restorationBytes, PROT_READ | PROT_WRITE | PROT_EXEC);
     if(status == MEMPROTECT_FAILED) {
-        LOGE("DERR", "::mprotect failed step 2");
+        LOGE("DERR", "::mprotect failed on step 2");
         return NULL;
     }
 
     LOGD("DDBG", "::Trampoline Set");
     return (p_void) dhc->restorationBytes;
-
 }
 
 void DHook(p_void origin, p_void detour, p_void * trampoline) { 
